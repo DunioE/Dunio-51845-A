ca273bc98509af713d0808889fdfd828e4e540ae
DunioE <126677297+DunioE@users.noreply.github.com>
2023-10-21 13:25:38 +0100

    1. NaMesh Movement
    Terminado o 5º Vídeo do tutorial 3.

--- Assets/BehaviourTree/RobberBehaviour.cs
+++ Assets/BehaviourTree/RobberBehaviour.cs
@@ -5,11 +5,14 @@ using UnityEngine.AI;
 
 public class RobberBehaviour : MonoBehaviour
 {
+    BehaviourTree tree;
     public GameObject diamond;
     public GameObject van;
     NavMeshAgent agent;
 
-    BehaviourTree tree;
+    public enum ActionState { IDLE, WORKING };
+    ActionState state = ActionState.IDLE;
+
 
     void Start()
     {
@@ -32,15 +35,34 @@ public class RobberBehaviour : MonoBehaviour
 
     public Node.Status GoToDiamond()
     {
-        agent.SetDestination(diamond.transform.position);
-        return Node.Status.SUCCESS;
+        return GoToLocation(diamond.transform.position);
     }
 
 
     public Node.Status GoToVan()
     {
-        agent.SetDestination(van.transform.position);
-        return Node.Status.SUCCESS;
+        return GoToLocation(van.transform.position);
+    }
+
+    Node.Status GoToLocation(Vector3 destination)
+    {
+        float distanceToTarget = Vector3.Distance(destination, this.transform.position);
+        if (state == ActionState.IDLE)
+        {
+            agent.SetDestination(destination);
+            state = ActionState.WORKING;
+        }
+        else if (Vector3.Distance(agent.pathEndPosition, destination) >= 2)
+        {
+            state = ActionState.IDLE;
+            return Node.Status.FAILURE;
+        }
+        else if (distanceToTarget < 2)
+        {
+            state = ActionState.IDLE;
+            return Node.Status.SUCCESS;
+        }
+        return Node.Status.RUNNING;
     }
 
 
